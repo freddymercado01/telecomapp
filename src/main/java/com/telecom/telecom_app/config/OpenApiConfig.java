7d3e97d70a07b44325f345e00ebc351cf729ee7e
frmercado <tu_frmercado@emcali.com.co>
2026-04-17 15:20:51 -0500

    fix(swagger): forzar https y habilitar encabezados

--- src/main/java/com/telecom/telecom_app/config/OpenApiConfig.java
+++ src/main/java/com/telecom/telecom_app/config/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,6 +16,11 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://telecomapp-production.up.railway.app")
+                                .description("Railway HTTPS")
+                ))
                 .info(new Info()
                         .title("Telecom App API")
                         .version("1.0.0")
