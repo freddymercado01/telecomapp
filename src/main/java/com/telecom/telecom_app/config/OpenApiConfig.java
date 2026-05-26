2d97ec31451fc319da4fc56f74a8a9686a5fb3ea
frmercado <tu_frmercado@emcali.com.co>
2026-05-26 13:58:42 -0500

    Fix/ esquema de seguridad JWT

--- src/main/java/com/telecom/telecom_app/config/OpenApiConfig.java
+++ src/main/java/com/telecom/telecom_app/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.telecom.telecom_app.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -35,6 +38,14 @@ public class OpenApiConfig {
                                 .url("https://www.telecom.com"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Ingresa el token JWT obtenido del endpoint POST /api/auth/login")));
     }
 }
